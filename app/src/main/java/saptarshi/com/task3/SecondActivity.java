f53314efb8478d14df9700b642bc6b3238ecd375
Saptarshi Roy Choudhuri <saptarshi.roychoudhuri@gmail.com>
2016-12-22 13:22:34 +0530

    Modified

--- app/src/main/java/saptarshi/com/task3/SecondActivity.java
+++ app/src/main/java/saptarshi/com/task3/SecondActivity.java
@@ -2,7 +2,6 @@ package saptarshi.com.task3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,8 +9,6 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        TextView editText = (TextView) findViewById(R.id.edit_message);
-        editText.setTextSize(30);
 
     }
 }
